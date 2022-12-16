05af2526eda6bf0004d42ea01a3b32cdcc7bda33
tenon <58762103+tenondvpn@users.noreply.github.com>
2022-12-16 09:32:07 +0800

    fix

--- net/net_utils.go
+++ net/net_utils.go
@@ -2,6 +2,19 @@ package net
 
 import (
 	"net"
+	"reflect"
+	"time"
+	"unsafe"
+
+	queue "github.com/smallnest/queue"
+)
+
+var (
+	MaxPackageSize = 1024 * 1024 * 2
+	IoThreadCount  = 4
+	IoPoolSize     = 32
+	BuffPoolSize   = IoThreadCount * (IoPoolSize + 1)
+	msgHandler     *MessageHandler
 )
 
 type EndPoint struct {
@@ -30,4 +43,41 @@ type EventItem struct {
 	removed    bool
 	packageLen int
 	parentConn *Connection
+}
+
+type QueueMsg struct {
+	queueId  uint16
+	msgType  uint16
+	data     []byte
+	conn     *net.Conn
+	msgBuf   *MsgBuffer
+	bufQueue *queue.LKQueue
+}
+
+type ConfigMsg struct {
+	queueId   uint16
+	msgType   uint16
+	txCluster string
+	data      []byte
+}
+
+type TcpCallback func(qMsg *QueueMsg) bool
+type ReceiverCallback func(msgId uint64, data []byte) int
+
+func TimeStampMilli() int64 {
+	return time.Now().UnixNano() / (1000 * 1000)
+}
+
+func Bytes2Str(slice []byte) string {
+	return *(*string)(unsafe.Pointer(&slice))
+}
+
+func Str2Bytes(s string) []byte {
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh := reflect.SliceHeader{
+		Data: sh.Data,
+		Len:  sh.Len,
+		Cap:  sh.Len,
+	}
+	return *(*[]byte)(unsafe.Pointer(&bh))
 }
