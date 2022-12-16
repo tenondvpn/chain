9a125235b4495ad18f7a7c25882da4d44ff15226
tenon <58762103+tenondvpn@users.noreply.github.com>
2022-12-16 14:21:11 +0800

    fix

--- net/net_proto_test.go
+++ net/net_proto_test.go
@@ -1,26 +1,26 @@
 package net
 
 import (
-	"log"
+	"testing"
 
 	"github.com/golang/protobuf/proto"
 )
 
-func main() {
+func TestProto(t *testing.T) {
 	test := &BroadcastParam{}
 	test.Type = 1
 
 	data, err := proto.Marshal(test)
 	if err != nil {
-		log.Fatal("marshaling error: ", err)
+		t.Errorf("marshaling error: ", err)
 	}
 	newTest := &BroadcastParam{}
 	err = proto.Unmarshal(data, newTest)
 	if err != nil {
-		log.Fatal("unmarshaling error: ", err)
+		t.Errorf("unmarshaling error: ", err)
 	}
 	// Now test and newTest contain the same data.
 	if test.GetType() != newTest.GetType() {
-		log.Fatalf("data mismatch %q != %q", test.GetType(), newTest.GetType())
+		t.Errorf("data mismatch %q != %q", test.GetType(), newTest.GetType())
 	}
 }
